d509d48f3d075267cd8a7b1411be96e788abe1dc
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-01-03 20:22:50 +0100

    feat: exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import webLanguages from './data/webLanguages'
 
 function App() {
+
+  const [selectedLanguage, setSelectedLanguage] = useState(webLanguages[0])
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <section className='container'>
+        <h1 className='text-center my-3'>Learn Web Development</h1>
+        <div>
+          {/* console.log(webLanguages); */}
+          {webLanguages.map((curLanguage) => (
+            <button key={curLanguage.id} className='my-3 me-3' onClick={() => setSelectedLanguage(curLanguage)}>
+              {curLanguage.name}
+            </button>
+          ))}
+        </div>
+        <div>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{selectedLanguage.name}</h5>
+              <p className="card-text">{selectedLanguage.description}</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
---
+++ src/data/webLanguages.js
@@ -0,0 +1,37 @@
+const webLanguages = [
+    {
+        id: 1,
+        name: "HTML",
+        description: "HTML (HyperText Markup Language) è il linguaggio di markup utilizzato per strutturare e presentare il contenuto delle pagine web."
+    },
+    {
+        id: 2,
+        name: "CSS",
+        description: "CSS (Cascading Style Sheets) è un linguaggio di stile usato per descrivere l'aspetto e il layout di un documento HTML."
+    },
+    {
+        id: 3,
+        name: "JavaScript",
+        description: "JavaScript è un linguaggio di scripting che consente di creare contenuti interattivi e dinamici per le pagine web."
+    },
+    {
+        id: 4,
+        name: "PHP",
+        description: "PHP (Hypertext Preprocessor) è un linguaggio lato server utilizzato per lo sviluppo web e per creare pagine web dinamiche."
+    },
+    {
+        id: 5,
+        name: "Python",
+        description: "Python è un linguaggio di programmazione versatile che può essere utilizzato per il web con framework come Django e Flask."
+    },
+    {
+        id: 6,
+        name: "TypeScript",
+        description: "TypeScript è un linguaggio basato su JavaScript che aggiunge tipizzazione statica, rendendo il codice più robusto e manutenibile."
+    }
+];
+
+console.log(webLanguages);
+
+
+export default webLanguages;
